308a6dab04b8e93274d890a6bc6743e4abb7e96a
Nursurtan <arapovnur23@gmail.com>
2022-01-29 21:22:48 +0700

    Added Stepper and isSelected

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -9,23 +9,51 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
-
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-
+    var tip = 0.10
+    var numberOfPeople = 2
+    
     @IBAction func tipChanged(_ sender: UIButton) {
+        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        // Make the button that triggered the IBAction selected.
+        sender.isSelected = true
+        // Get the current title of the button that was pressed.
+        let buttonTitle = sender.currentTitle!
+        // Remove the last character (%) from the title then turn it back into a String.
+        let buttonTitleMinusPercentSign = String(buttonTitle.dropLast())
+        // Turn the String into a Double
+        let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+        // Divide the percent expressed out of 100 into a decimal e.g. 10 become 0.1.
+        tip = buttonTitleAsANumber / 100
         
     }
     
+    
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        //Get the Stepper value using sender.value, round it down to a whole number then set it as the text in
+        // the splitNumberLabel
         
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        // Set of numberOfPeople property as the value of the stepper as a whole number.
+        numberOfPeople = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+
+        print(numberOfPeople)
+        
+        
+        
         
     }
     
--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var settingsLabel: UILabel!
     
