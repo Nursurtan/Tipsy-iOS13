5b35b57fa13b9f05b577d6b198f5dc103ef0e037
Nursurtan <arapovnur23@gmail.com>
2022-01-30 22:34:48 +0700

    Added to calculate the Bill

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -18,6 +18,8 @@ class CalculatorViewController: UIViewController {
     
     var tip = 0.10
     var numberOfPeople = 2
+    var billTotal = 0.0
+    
     
     @IBAction func tipChanged(_ sender: UIButton) {
         
@@ -35,7 +37,8 @@ class CalculatorViewController: UIViewController {
         let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
         // Divide the percent expressed out of 100 into a decimal e.g. 10 become 0.1.
         tip = buttonTitleAsANumber / 100
-        
+        //Dismiss the keyboard when the user chooses one of the tip values.
+        billTextField.endEditing(true)
     }
     
     
@@ -48,12 +51,28 @@ class CalculatorViewController: UIViewController {
         numberOfPeople = Int(sender.value)
     }
     
+    
+    
     @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        self.performSegue(withIdentifier: "goToResult", sender: self)
 
-        print(numberOfPeople)
+        //Get the text the user typed in the billTextField
+        let bill = billTextField.text!
         
-        
-        
+        //If the text is not an empty String ""
+        if bill != "" {
+            //Turn the bill from a String e.g. "123.50" to an actual String with decimal places.
+            //e.g. 125.50
+            billTotal = Double(bill)!
+            
+            //Multiply the bill by the tip percentage and divide by the number of people to split the bill.
+            let result = billTotal * (1 + tip) / Double(numberOfPeople)
+            
+            //Round the result to 2 decimal places and turn it into a String.
+            let resultTo2DecimalPlaces = String(format: "%.2f", result)
+            print(resultTo2DecimalPlaces)
+        }
         
     }
     
